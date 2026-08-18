48898bfdf0f9a7cd48a8eaebfa302b9bc35003e6
knodelt <thomasknodel@hotmail.de>
2026-08-18 14:07:26 +0200

    Add QRPass offline shell

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='qrpass-shell-v0.1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();if(new URL(event.request.url).origin===location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
+});
