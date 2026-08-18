3980961f4acdcd447360671d471f96dbf5ef1ee8
knodelt <thomasknodel@hotmail.de>
2026-08-18 19:37:03 +0200

    release: add central legal operator configuration

---
+++ legal-config.js
@@ -0,0 +1,12 @@
+window.QRPassLegalConfig = {
+  operatorName: 'Thomas Knodel',
+  businessName: '',
+  street: '',
+  postalCode: '',
+  city: '',
+  country: 'Deutschland',
+  email: '',
+  phone: '',
+  vatId: '',
+  updatedAt: '18.08.2026'
+};
